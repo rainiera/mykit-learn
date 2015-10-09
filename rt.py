6640d1695de28e80a4f285f5ae0114f4af1db291
Rainier Ababao <rainier.ababao@gmail.com>
2015-10-09 11:58:05 -0500

    Write speed tests to file

--- rt.py
+++ rt.py
@@ -14,17 +14,17 @@ def rethink_speed_test_list():
     t1 = time.time()
     five_star_reviews = [document for document in cursor]
     t2 = time.time()
-    print ('time to make db conn: {0}').format(t1-t0)
-    print ('time to generate list: {0}').format(t2-t1)
-    print ('num 5-star reviews {0}').format(len(five_star_reviews))
+    return ('time to make db conn: {0}\n').format(t1-t0) + ('time to generate list: {0}\n').format(t2-t1) + ('num 5-star reviews {0}').format(len(five_star_reviews))
 
 def rethink_speed_test_count():
     t0 = time.time()
     count = r.table('reviews').count(lambda review: review['stars'] == 5).run()
     t1 = time.time()
-    print ('time to count 5-star reviews: {0}').format(t1-t0)
-    print ('num 5-star reviews {0}').format(count)
+    return ('time to count 5-star reviews: {0}\n').format(t1-t0) + ('num 5-star reviews {0}').format(count)
 
 
 if __name__ == "__main__":
     rethink_speed_test_count()
+    f = open('speed_test.txt', 'w')
+    f.write(rethink_speed_test_count() + "\n\n" + rethink_speed_test_list())
+
